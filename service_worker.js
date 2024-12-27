f3ab61b3f06f87efd6142fe4e45f1fd81a68d70f
pkali <pawel.kalinowski@gmail.com>
2024-12-27 10:20:52 -0500

    PWA fixes

--- service_worker.js
+++ service_worker.js
@@ -1,51 +1,23 @@
-self.addEventListener("install", function(event) {
-    event.waitUntil(preLoad());
-  });
-  
-  var preLoad = function(){
-    console.log("Installing web app");
-    return caches.open("offline").then(function(cache) {
-      console.log("caching index and important routes");
-      return cache.addAll(["/", "/offline.html"]);
-    });
-  };
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(checkResponse(event.request).catch(function() {
-      return returnFromCache(event.request);
-    }));
-    event.waitUntil(addToCache(event.request));
-  });
-  
-  var checkResponse = function(request){
-    return new Promise(function(fulfill, reject) {
-      fetch(request).then(function(response){
-        if(response.status !== 404) {
-          fulfill(response);
-        } else {
-          reject();
-        }
-      }, reject);
-    });
-  };
-  
-  var addToCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log(response.url + " was cached");
-        return cache.put(request, response);
-      });
-    });
-  };
-  
-  var returnFromCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return cache.match(request).then(function (matching) {
-       if(!matching || matching.status == 404) {
-         return cache.match("offline.html");
-       } else {
-         return matching;
-       }
-      });
-    });
-  };
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("converto-v1").then((cache) => {
+            return cache.addAll([
+                "/converto/",             // or "/converto/index.html"
+                "/converto/script.js",
+                "/converto/style.css",
+                "/converto/manifest.json",
+                "/converto/converto.png",
+                "/converto/offline.html"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            // If there's a cached response, return it; otherwise, fetch from network
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
